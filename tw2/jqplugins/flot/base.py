28120d68948385b7b24c0a906034b318dede8e82
Ralph Bean <ralph.bean@gmail.com>
2011-04-20 15:14:37 -0400

    Cleaning

--- tw2/jqplugins/flot/base.py
+++ tw2/jqplugins/flot/base.py
@@ -10,9 +10,5 @@ flot_js = twjq_c.jQueryPluginJSLink(
     modname='tw2.jqplugins.flot',
     subdir = '',
 )
-flot_utils_js = twc.JSLink(
-    modname='tw2.jqplugins.flot',
-    filename='static/js/flot-utils.js',
-)
 
-__all__ = ['flot_js', 'flot_utils_js']
+__all__ = ['flot_js']
