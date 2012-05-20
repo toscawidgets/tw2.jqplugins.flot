eb604392e3718164618295e707784e1a345561ed
Ralph Bean <rbean@redhat.com>
2012-05-20 16:54:48 -0400

    PEP8 in samples.py.

--- tw2/jqplugins/flot/samples.py
+++ tw2/jqplugins/flot/samples.py
@@ -2,14 +2,17 @@
 from widgets import FlotWidget
 
 import math
+
+
 def f(x):
-    return x/math.pi
+    return x / math.pi
+
 
 class DemoFlot(FlotWidget):
     data = [
         {
-            'data' : [[f(i), math.sin(f(i))] for i in range(20)],
-            'label' : "sin(x)"
+            'data': [[f(i), math.sin(f(i))] for i in range(20)],
+            'label': "sin(x)"
         }
     ]
     label = "sin plot"
